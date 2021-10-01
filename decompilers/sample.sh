fd7b23774fbad7b13d6b6afa55a27bc686f7c45c
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2021-10-01 20:11:16 +0200

    Add files via upload

---
+++ decompilers/sample.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#NAME: sample
+#RATE: perfect
+#INST: sample.py
+
+
+install () {
+	
+	echo "Installing 3rd party software"
+
+}
+
+
+run () {
+	
+	echo "Running decompiler"
+
+}
+
+echo $1
