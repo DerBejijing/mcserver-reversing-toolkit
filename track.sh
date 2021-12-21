290ca8dd2d012aedfffd4ee143565f9748263946
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2021-12-21 21:13:00 +0100

    Update track.sh

--- track.sh
+++ track.sh
@@ -21,13 +21,16 @@ COLOR_RESET='\033[0m'
 
 
 
+# computes the checksum for all decompiled java files
+# all checksums get then stored in the $FILESUMS file
 initialize () {
 
-	if test -f $FILESUMS; then
-		rm $FILESUMS
-	fi
-
+	# (re)create file
+	rm -f $FILESUMS
 	touch $FILESUMS
+
+	# compute checksum using shasum
+	# remove spaces
 	find $DIR_DECOMPILED_JAR -name "*.java" | xargs shasum | tr -s '[:space:]' > $FILESUMS
 
 	exit
@@ -35,21 +38,33 @@ initialize () {
 }
 
 
+# computes the checksum for all decompiled java files to
+#  compare them against the stored ones
+# the temporary checksums get stored in $TMP_FILESUMS, which gets recreated every time
 track () {
 
-	if test -f $TMP_FILESUMS; then
-		rm $TMP_FILESUMS
-	fi
+	# (re)create file
+	rm -f $TMP_FILESUMS
 	touch $TMP_FILESUMS
+
+	# compute checksum using shasum
+	# remove spaces
 	find $DIR_DECOMPILED_JAR -name "*.java" | xargs shasum | tr -s '[:space:]' > $TMP_FILESUMS
 
 	# empties the filetracker. needed/useful?
-	rm $FILETRACKER
+	# I don't know
+	rm -f $FILETRACKER
 	touch $FILETRACKER
 
+	# iterate over every line in $TMP_FILESUMS
+	# check if every line appears in $FILESUMS
+	# if not, the file has changed and thus, must be recompiled
 	while read line; do
 
+		# check if the line appears in $FILESUMS using grep
 		CONTAINS=$(cat $FILESUMS | grep "$line" | wc -l)
+
+		# If it is not included (which means it has changed) it gets added to the filetracker
 		if [[ $CONTAINS = "0" ]]; then
 			echo "${line}"
 			substrings=( $line )
@@ -64,15 +79,19 @@ track () {
 
 
 
+# only run if the project has been initialized
 if ! [[ $(${SCRIPT_GET_INITIALIZED}) = "ok" ]]; then
 	echo -e "${COLOR_RED}Some directories are missing, try running ${COLOR_LIGHT_BLUE}./setup${COLOR_RESET}"
 	exit 0
 fi
 
+# check arguments
+# "--init" only recreates the $FILESUMS file
 if [[ -n $1 ]]; then
 	if [[ $1 = "--init" ]]; then
 		initialize
 	fi
 fi
 
+# no arguments supplied -> track gets called
 track
