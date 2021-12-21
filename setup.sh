1cc17bce5718dae608feacf9aa3b212c7ff9c79d
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2021-12-21 21:01:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ DIR_SERVER_JAR=./server_jar
 DIR_DECOMPILED_JAR=./decompiled_jar
 DIR_PATCHED_JAR=./patched_jar
 
+DIR_INSTALLERS=${DIR_TOOL}/server_installers
 DIR_DECOMPILERS=${DIR_TOOL}/decompilers
 DIR_DECOMPILERS_BIN=${DIR_DECOMPILERS}/bin
 DIR_UTILS=${DIR_TOOL}/utils
@@ -24,6 +25,8 @@ COLOR_RESET='\033[0m'
 
 setup_files () {
 
+	# delete all directorys
+	# no matter whether or not they actually exist
 	rm -rf $DIR_PATCHED_JAR
 	rm -rf $DIR_SERVER_JAR
 	rm -rf $DIR_DECOMPILED_JAR
@@ -33,26 +36,36 @@ setup_files () {
 	rm -f $FILETRACKER
 	rm -f $SERVER_ARGS
 
+	# (re)create all directorys
 	mkdir $DIR_PATCHED_JAR
 	mkdir $DIR_SERVER_JAR
 	mkdir $DIR_DECOMPILED_JAR
 	mkdir $DIR_TMP
 	mkdir $DIR_DECOMPILERS_BIN
 
+	# (re)create all files necessary to use the tool
 	touch $FILESUMS
 	touch $FILETRACKER
 	touch $SERVER_ARGS
 
-
+	# set the executable bit for all scripts in the root directory
+	#  of the project
 	chmod +x *.sh
 
 	TMP_LOC=$(pwd)
 	
+	# set the executable bit for all utility-scripts
 	cd $DIR_UTILS
 	chmod +x *.sh
 	cd $TMP_LOC
 
+	# set the executable bit for all decompiler-scripts
 	cd $DIR_DECOMPILERS
+	chmod +x *.sh
+	cd $TMP_LOC
+
+	# set the executable bit for all server-installer-scripts
+	cd $DIR_INSTALLERS
 	chmod +x *.sh
 	cd $TMP_LOC
 
@@ -60,14 +73,16 @@ setup_files () {
 
 
 
-
+# check flags for "--confirm"
+# this will skip the prompt asking the user whether or not they accept
+#  deletion and recreation of all files and directorys
 if [[ $1 = "--confirm" ]]; then
 	CONTINUE=yes
 else
 	read -r -p "This will setup all files / folders and delete existing. Are you sure you want to continue? [y/n]" CONTINUE
 fi
 
-
+# only run setup_files if the user agrees
 if [[ $CONTINUE =~ ^(yes|y)$ ]]; then
 	setup_files
 	exit 0
